91707133b9ef33ec38c9d80e1178f11fd2f31188
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-24 19:32:55 -0400

    debugging

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
