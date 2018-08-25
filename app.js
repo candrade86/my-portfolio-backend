24b88f024ea84edb5a594d15ceeab499f845bb70
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-25 14:57:50 -0400

    wip debugging

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 // });
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000/');
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000/#');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000/'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000/#'}));
 
 app.use('/api', index);
 
