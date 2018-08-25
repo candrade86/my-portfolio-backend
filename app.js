9070ba0a5dba28edaf3cc10d7c78aab0a8af5388
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-25 14:41:54 -0400

    changed cors setting to fix error when trying to access db from heroku

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ var index = require('./routes/index.js');
 
 var app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: '*'}));
+
+// app.use(cors({credentials: true, origin: '*'}));
 
 app.use('/api', index);
 
