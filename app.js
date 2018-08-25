9ddb28c0d4796581fbbca362fa6c8aec83ac0170
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-25 14:56:11 -0400

    debugging cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(cors({credentials: true, origin: '*'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000/'}));
 
 app.use('/api', index);
 
