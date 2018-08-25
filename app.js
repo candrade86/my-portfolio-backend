f9c0a3aeeb8efa4561d8525ee92376181f30fd34
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-25 17:26:38 -0400

    debugging cors

--- app.js
+++ app.js
@@ -15,20 +15,20 @@ var app = express();
 //   next();
 // });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000/#');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", 'http://localhost:3000/#');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000/#'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 app.use('/api', index);
 
