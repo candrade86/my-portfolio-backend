c9ec75411ec9ef2966a1be3d7f4c4361b2a2b835
Carlos Andrade <carlos.andrade86@outlook.com>
2018-08-26 16:23:43 -0400

    testing a debug

--- app.js
+++ app.js
@@ -32,25 +32,6 @@ app.use(cookieParser());
 
 app.use('/api', index);
 
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`API RUNNING ON ${port}`))
 
